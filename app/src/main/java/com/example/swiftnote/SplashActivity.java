7e82b8b46dc753478c49124125aec680145a529e
Debjit Saha <debjitsaha606@gmail.com>
2025-08-11 09:22:17 +0530

    splash screen error fixed

--- app/src/main/java/com/example/swiftnote/SplashActivity.java
+++ app/src/main/java/com/example/swiftnote/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.example.swiftnote;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +21,15 @@ public class SplashActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
+
         });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            }
+        },1000);
     }
 }
