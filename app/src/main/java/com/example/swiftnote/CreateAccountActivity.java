daf8f9195bd38b39bb04b9181ac1472c4e2cc643
Debjit Saha <debjitsaha606@gmail.com>
2025-08-11 10:38:42 +0530

    account create logic done

--- app/src/main/java/com/example/swiftnote/CreateAccountActivity.java
+++ app/src/main/java/com/example/swiftnote/CreateAccountActivity.java
@@ -1,15 +1,33 @@
 package com.example.swiftnote;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.regex.Pattern;
+
+public class CreateAccountActivity extends AppCompatActivity {
+    EditText emailEditText,passwordEditText,confirmPasswordEditText;
+    Button createAccountBtn;
+    ProgressBar progressBar;
+    TextView loginBtnTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +38,82 @@ public class CreateAccountActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
+        createAccountBtn = findViewById(R.id.create_account_btn);
+        progressBar = findViewById(R.id.progress_bar);
+        loginBtnTextView = findViewById(R.id.login_text_view_btn);
+
+        createAccountBtn.setOnClickListener(v->createAccount());
+        loginBtnTextView.setOnClickListener(v->finish());
+
+    }
+
+    void createAccount(){
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String confirmPassword = confirmPasswordEditText.getText().toString();
+
+        boolean isValidated = validateData(email,password,confirmPassword);
+        if(!isValidated){
+            return;
+        }
+        createAccountInFirebase(email,password);
+    }
+
+    void createAccountInFirebase(String email,String password){
+        changeInProgress(true);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(CreateAccountActivity.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        changeInProgress(false);
+                        if(task.isSuccessful()){
+                            Toast.makeText(CreateAccountActivity.this,"Succesfully account created",Toast.LENGTH_SHORT).show();
+                            assert firebaseAuth.getCurrentUser() != null;
+                            firebaseAuth.getCurrentUser().sendEmailVerification();
+                            firebaseAuth.signOut();
+                            finish();
+                        }else{
+                            Toast.makeText(CreateAccountActivity.this,task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+
+    }
+
+    void changeInProgress(boolean isProgress){
+        if(isProgress){
+            progressBar.setVisibility(View.VISIBLE);
+            createAccountBtn.setVisibility(View.GONE);
+        }else{
+            progressBar.setVisibility(View.GONE);
+            createAccountBtn.setVisibility(View.VISIBLE);
+        }
+    }
+    Boolean validateData(String email,String password,String confirmPassword){
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+           emailEditText.setError("Email is invalid");
+           return false;
+        }
+        if(password.length() < 6){
+            passwordEditText.setError("Password length is invalid");
+            return false;
+        }
+        if(password.isEmpty()){
+            passwordEditText.setError("Password can not be empty");
+            return false;
+        }
+        if(!password.equals(confirmPassword)){
+            confirmPasswordEditText.setError("Password not matched");
+            return false;
+        }
+
+        return true;
     }
 }
